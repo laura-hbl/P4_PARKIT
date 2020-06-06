a91e49981d44f12a9987a9ffd1bf912d5e750465
Laura <laura_habdul@hotmail.fr>
2020-06-06 02:50:24 +0200

    add unit tests for ParkingSpotDAO class

---
+++ src/test/java/com/parkit/parkingsystem/unit/ParkingSpotDAOTest.java
@@ -0,0 +1,92 @@
+package com.parkit.parkingsystem.unit;
+
+import com.parkit.parkingsystem.config.DataBaseConfig;
+import com.parkit.parkingsystem.constants.ParkingType;
+import com.parkit.parkingsystem.dao.ParkingSpotDAO;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ParkingSpotDAOTest {
+
+    private static ParkingSpotDAO parkingSpotDAO;
+
+    @Mock
+    private static DataBaseConfig dataBaseConfig;
+    @Mock
+    private static Connection connection;
+    @Mock
+    private static PreparedStatement preparedStatement;
+    @Mock
+    private static ResultSet resultSet;
+
+
+    @BeforeEach
+    public void setUpPerTest() throws Exception {
+        when(dataBaseConfig.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(any(String.class))).thenReturn(preparedStatement);
+        parkingSpotDAO = new ParkingSpotDAO(dataBaseConfig);
+    }
+
+    @Test
+    public void givenParkingTypeCar_whenGetNextAvailableSpot_thenNextAvailableSpotMatchesExpected() throws Exception {
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true);
+        when(resultSet.getInt(1)).thenReturn(2);
+
+        int nextAvailableSpot = parkingSpotDAO.getNextAvailableSpot(ParkingType.CAR);
+
+        verify(preparedStatement).setString(1, ParkingType.CAR.toString());
+        verify(preparedStatement).executeQuery();
+        verify(resultSet).next();
+        verify(resultSet).getInt(1);
+        assertThat(nextAvailableSpot).isEqualTo(2);
+    }
+
+    @Test
+    public void givenParkingTypeCar_whenGetNextAvailableSpotIsInvalid_thenNegativeResultReturned() throws Exception {
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(false);
+
+        int nextAvailableSpot = parkingSpotDAO.getNextAvailableSpot(ParkingType.CAR);
+
+        assertThat(nextAvailableSpot).isEqualTo(-1);
+    }
+
+    @Test
+    public void givenAParkingSpot_whenUpdateParking_thenParkingSpotIsUpdated() throws Exception {
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, true);
+        when(preparedStatement.executeUpdate()).thenReturn(1);
+
+        boolean isParkingUpdated = parkingSpotDAO.updateParking(parkingSpot);
+
+        InOrder inOrder = inOrder(preparedStatement);
+        inOrder.verify(preparedStatement).setBoolean(1,parkingSpot.isAvailable());
+        inOrder.verify(preparedStatement).setInt(2, parkingSpot.getNumber());
+        inOrder.verify(preparedStatement).executeUpdate();
+        assertThat(isParkingUpdated).isEqualTo(true);
+    }
+
+    @Test
+    public void givenAParkingSpot_whenUpdateParkingNotExecuted_thenParkingSpotNotUpdated() throws Exception {
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, true);
+        when(preparedStatement.executeUpdate()).thenReturn(0);
+
+        boolean isParkingUpdated = parkingSpotDAO.updateParking(parkingSpot);
+
+        assertThat(isParkingUpdated).isEqualTo(false);
+    }
+}
